89543575393e1afa65a4119fe06d36f930bb24a3
zulus23 <zulus23@gmail.com>
2017-10-19 16:28:16 +0300

    add login

--- WebApp/src/main/java/ru/zhukov/recoverdebt/config/SecurityConfig.java
+++ WebApp/src/main/java/ru/zhukov/recoverdebt/config/SecurityConfig.java
@@ -1,20 +1,27 @@
 package ru.zhukov.recoverdebt.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@Configuration
+/*@EnableWebSecurity*/
+/*@Order(Ordered.HIGHEST_PRECEDENCE)*/
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/css/**", "/index").permitAll()
-                .antMatchers("/**").hasRole("USER")
+                .antMatchers("/").permitAll()
+
+                .antMatchers("/resources/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .and()
-                .httpBasic();
+                 .formLogin()
+                 .loginPage("/login")
+                 .permitAll();
     }
 }
--- WebApp/src/main/java/ru/zhukov/recoverdebt/web/RunWebApplication.java
+++ WebApp/src/main/java/ru/zhukov/recoverdebt/web/RunWebApplication.java
@@ -1,16 +1,29 @@
 package ru.zhukov.recoverdebt.web;
 
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import ru.zhukov.recoverdebt.domain.User;
 
 
 @Controller
-@EnableAutoConfiguration
-public class RunWebApplication {
+@SpringBootApplication(scanBasePackages = "ru.zhukov.recoverdebt")
+public class RunWebApplication  extends WebMvcConfigurerAdapter {
+
 
 
     @RequestMapping("/")
@@ -19,8 +32,22 @@ public class RunWebApplication {
         User user = new User();
         return String.format("Test run application for user %s %n",user);
     }
+    @GetMapping("/login")
+    public String login(){
+        return "/login";
+    }
+
+
+    @RequestMapping("/users")
+    @ResponseBody
+    public String user(){
+        return "user";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RunWebApplication.class,args);
     }
+
+
+
 }
